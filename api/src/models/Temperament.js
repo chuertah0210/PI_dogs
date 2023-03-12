b108d3504194a1b89dfb66194de1411783b31dea
chuertah0210 <carloshuertahormazabal@gmail.com>
2023-03-12 19:49:39 -0300

    BD

--- api/src/models/Temperament.js
+++ api/src/models/Temperament.js
@@ -3,31 +3,15 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
+  sequelize.define('temperament', {
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true,
       allowNull: false
     },
-    imagen: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
     nombre: {
       type: DataTypes.STRING,
-      allowNull: false
-    },
-    altura: {
-      type: DataTypes.FLOAT,
-      allowNull: false
-    },
-    peso: {
-      type: DataTypes.FLOAT,
-      allowNull: false
-    },
-    años_de_vida: {
-      type: DataTypes.INTEGER,
       allowNull: false
     }
   });
